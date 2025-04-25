d4b9b9b059c1c500851eb188944f155b7ade836f
Juuuuuuuuno <dlwnsgh0531@gmail.com>
2025-04-25 22:39:46 +0900

    중간 저장

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ export default function Home() {
 return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Link href="/FarmInfo" asChild>
-        <Button title="농장 정보 보기" />
+        <Button title="농장 정보 보기 " />
     </Link>
     </View> 
     );
